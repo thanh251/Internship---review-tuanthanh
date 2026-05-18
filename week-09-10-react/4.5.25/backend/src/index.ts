95f94697095b55262b89e0eb8754d88e9c7c1534
thanh251 <tuanthanh251207@gmail.com>
2026-05-18 11:25:13 +0700

    gdf

--- week-09-10-react/4.5.25/backend/src/index.ts
+++ week-09-10-react/4.5.25/backend/src/index.ts
@@ -1,33 +1,29 @@
 import express from 'express';
-import userRouter from  './routes/user.route.js'
-//import bodyParser from 'body-parser'
+import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import categoryRouter from './routes/category.controller.js'
 import postRouter from './routes/post.route.js'
 import commentRouter from './routes/comment.route.js'
 import cors from 'cors'
+
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
-// app.use(bodyParser)
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://internship-review-tuanthanh.vercel.app'  // bỏ dấu / ở cuối
+  ],
   credentials: true
-})) //để gọi api
+}))
 
 app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/categories', categoryRouter)
 app.use('/api/posts', postRouter)
 app.use('/api', commentRouter)
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://internship-review-tuanthanh.vercel.app/' // thêm sau khi có link Vercel
-  ],
-  credentials: true
-}))
